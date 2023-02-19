3b985f60dfb40ebd3dc097d533f0c3b8a54415a0
Kinkando <TY_Sage@hotmail.com>
2023-02-19 18:15:04 +0700

    feat: add login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { Button, Card, Label, Input, ButtonGroup, InputAddon, ToolbarButton } from 'flowbite-svelte';
+    let showPassword = false;
+</script>
+
+<div class="w-screen h-screen flex overflow-y-auto">
+    <!-- <div class="w-full bg-primary px-6 sm:max-md:hidden"> -->
+    <div class="w-full bg-primary px-6 max-[900px]:hidden">
+        <div class="relative top-1/2 -translate-y-1/2 overflow-y-auto">
+            <img class="w-2xl m-auto" src="images/SU-WEBBOARD-ICON.png" alt="">
+            <img class="w-2xl m-auto" src="images/SU-WEBBOARD-TEXT.png" alt="">
+        </div>
+    </div>
+    <div class="w-full max-[900px]:bg-primary min-[901px]:bg-gray-200 p-4">
+        <Card class="relative m-auto top-1/2 -translate-y-1/2 overflow-y-auto">
+            <h1 class="text-center text-2xl mb-4 uppercase">Login</h1>
+            <Label class="space-y-2">
+                <span>Username</span>
+                <Input type="email" placeholder="Enter your username">
+                </Input>
+            </Label>
+            <Label class="space-y-2 mt-4">
+                <span>Password</span>
+                <Input type={showPassword ? 'text' : 'password'} placeholder="Enter your password" size="md">
+                    <button slot="right" on:click={() => showPassword = !showPassword} class="pointer-events-auto">
+                        {#if showPassword}
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
+                        {:else}
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" /></svg>
+                        {/if}
+                    </button>
+                </Input>
+            </Label>
+            <Button type="submit" class="mt-6 uppercase" color="green">Confirm</Button>
+        </Card>
+    </div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+  ],
 
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#40826D'
+      },
+      screens: {
+        ...defaultTheme.screens,
+      }
+    }
   },
 
-  plugins: []
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  darkMode: 'class',
 };
 
 module.exports = config;
