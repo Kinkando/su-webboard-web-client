3ad536e656001568e8a2cad16348c97a7c30ca5d
Kinkando <TY_Sage@hotmail.com>
2023-02-19 18:39:50 +0700

    feat: add firebase authen signin

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,9 +1,27 @@
 <script>
-    import { Button, Card, Label, Input, ButtonGroup, InputAddon, ToolbarButton } from 'flowbite-svelte';
+    import { Button, Card, Label, Input, Spinner } from 'flowbite-svelte';
+	import { signinFirebase } from '@services/firebase';
     let showPassword = false;
+    let isLoading = false;
+
+    let username = "";
+    let password = "";
+
+    const signin = async() => {
+        if (!username.length || !password.length) { return }
+        isLoading = true;
+        const resp = await signinFirebase(username, password);
+        isLoading = false;
+    }
 </script>
 
-<div class="w-screen h-screen flex overflow-y-auto">
+{#if isLoading}
+    <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20">
+        <Spinner color="green" size="8" />
+    </div>
+{/if}
+
+<div class="w-screen h-screen flex overflow-y-auto {isLoading ? 'brightness-50': ''}">
     <!-- <div class="w-full bg-primary px-6 sm:max-md:hidden"> -->
     <div class="w-full bg-primary px-6 max-[900px]:hidden">
         <div class="relative top-1/2 -translate-y-1/2 overflow-y-auto">
@@ -16,12 +34,30 @@
             <h1 class="text-center text-2xl mb-4 uppercase">Login</h1>
             <Label class="space-y-2">
                 <span>Username</span>
-                <Input type="email" placeholder="Enter your username">
-                </Input>
+                <Input
+                    type="email"
+                    placeholder="Enter your username"
+                    bind:value={username}
+                    on:keydown={event => {
+                        if (event.key === 'Enter') {
+                            signin();
+                        }
+                    }}
+                ></Input>
             </Label>
             <Label class="space-y-2 mt-4">
                 <span>Password</span>
-                <Input type={showPassword ? 'text' : 'password'} placeholder="Enter your password" size="md">
+                <Input
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Enter your password"
+                    size="md"
+                    bind:value={password}
+                    on:keydown={event => {
+                        if (event.key === 'Enter') {
+                            signin();
+                        }
+                    }}
+                >
                     <button slot="right" on:click={() => showPassword = !showPassword} class="pointer-events-auto">
                         {#if showPassword}
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
@@ -31,7 +67,7 @@
                     </button>
                 </Input>
             </Label>
-            <Button type="submit" class="mt-6 uppercase" color="green">Confirm</Button>
+            <Button type="submit" class="mt-6 uppercase" color="green" on:click={signin} disabled={!username.length || !password.length}>Confirm</Button>
         </Card>
     </div>
 </div>
---
+++ src/services/firebase.ts
@@ -0,0 +1,40 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+
+const env = import.meta.env;
+
+interface FirebaseConfiguration {
+  readonly apiKey: string
+  readonly authDomain: string
+  readonly projectId: string
+  readonly storageBucket: string
+  readonly messagingSenderId: string
+  readonly appId: string
+  readonly measurementId: string
+}
+
+const firebaseConfig: FirebaseConfiguration = {
+    apiKey: env.VITE_FIREBASE_API_KEY,
+    authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: env.VITE_FIREBASE_APP_ID,
+    measurementId: env.VITE_FIREBASE_MEASUREMENT_ID,
+}
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase)
+auth.useDeviceLanguage()
+
+export async function signinFirebase(username: string, password: string) {
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, username, password)
+        const idToken = (userCredential as any)._tokenResponse.idToken
+        // const email = userCredential.user.email || "";
+
+        return idToken
+    } catch(err) {
+        return undefined
+    }
+}
