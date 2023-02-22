e23b64b062fca01aa1433b78844d6f9ea3e47e5f
Kinkando <TY_Sage@hotmail.com>
2023-02-22 11:48:24 +0700

    refactor: logout separate goto out of logic revoke token

--- src/routes/admin-portal/+layout.svelte
+++ src/routes/admin-portal/+layout.svelte
@@ -32,9 +32,7 @@
 
     let isSidebarExpand = false;
 
-    const signout = () => {
-        goto("/login")
-    }
+    const signout = () => localStorage.clear();
 
     const hideSidebar = () => isSidebarExpand = false
 </script>
@@ -66,14 +64,14 @@
         <nav class="bottom-nav">
             <div class="p-2">
                 <!-- svelte-ignore a11y-click-events-have-key-events -->
-                <div class="transition-all duration-200 flex items-center p-2 rounded-md hover:text-[#40826D] hover:shadow-md hover:bg-white text-white cursor-pointer" on:click={signout}>
+                <a class="transition-all duration-200 flex items-center p-2 rounded-md hover:text-[#40826D] hover:shadow-md hover:bg-white text-white cursor-pointer" on:click={signout} href="/login">
                     <span>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                         </svg>
                     </span>
                     <span class="ml-2">Logout</span>
-                </div>
+                </a>
             </div>
         </nav>
     </aside>
