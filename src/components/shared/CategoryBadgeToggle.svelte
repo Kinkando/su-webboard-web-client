eeb980068e98090c8e2d623e73483a175228dc4e
Kinkando <TY_Sage@hotmail.com>
2023-02-22 20:42:15 +0700

    feat: draw UI of new forum page

---
+++ src/components/shared/CategoryBadgeToggle.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Category } from "@models/category";
+
+    export let isActive = false;
+    export let category: Category;
+</script>
+
+<button class="transition-all ease-in duration-150 relative outline outline-1 rounded-md p-1 w-fit m-1 {isActive ? '!text-white' : '!bg-white'}" style="background-color: {category.categoryHexColor}; outline-color: {category.categoryHexColor}; color: {category.categoryHexColor}" on:click={() => isActive = !isActive}>
+    <div class="flex items-center">
+        <div class="rounded-full w-5 h-5 bg-white mr-1 relative border-[1px]" style="border-color: {category.categoryHexColor}">
+            {#if isActive}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="w-5 h-5 pr-0.5" style="color: {category.categoryHexColor};">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                </svg>
+            {/if}
+        </div>
+        {category.categoryName}
+    </div>
+</button>
--- src/routes/forum/+page.svelte
+++ src/routes/forum/+page.svelte
@@ -1,1 +1,70 @@
-New Forum Page
+<script lang="ts">
+    import { onMount } from 'svelte';
+	import { Button, Input, Label, Textarea } from 'flowbite-svelte';
+	import type { Category } from '@models/category';
+	import { getAllCategories } from '@services/category';
+	import CategoryBadgeToggle from '@components/shared/CategoryBadgeToggle.svelte';
+
+    interface CategoryToggle {
+        categoryUUID: string
+        isActive: boolean
+    }
+
+    let title = "";
+    let description = "";
+    let categoryToggles: CategoryToggle[] = [];
+
+    let fileInput: HTMLInputElement;
+    let files: FileList;
+
+    $: if(files) {
+        console.log(...files)
+    }
+
+    let categories: Category[] = [];
+    onMount(async () => {
+        categories = await getAllCategories()
+        categories.forEach(category => categoryToggles.push({categoryUUID: category.categoryUUID, isActive: false}))
+    })
+</script>
+
+<div class="bg-white w-full rounded-md shadow-lg p-4 sm:p-6">
+    <Label for="title" class="space-y-2">
+        <span>Title</span>
+        <Input type="text" id="title" class="placeholder-gray-300" placeholder="Enter a title ..." required bind:value={title} />
+    </Label>
+
+    <Label for="description" class="space-y-2 mt-4">
+        <span>Description</span>
+        <Textarea id="description" class="placeholder-gray-300 min-h-[150px]" placeholder="Enter a description ..." required bind:value={description} />
+    </Label>
+
+    <Label for="category" class="space-y-2 mt-4">
+        <span>Categories</span>
+        <div id="category" class="w-full p-2.5 border text-sm rounded-lg bg-gray-50 text-gray-900 overflow-x-hidden flex flex-wrap">
+            {#if categories.length}
+                {#each categories as category, index}
+                    <CategoryBadgeToggle {category} bind:isActive={categoryToggles[index].isActive} />
+                {/each}
+            {:else}
+                {#each Array(5) as _}
+                    <!-- Skeleton Load CategoryBadgeToggle -->
+                    <div class="animate-pulse ease-in duration-200">
+                        <div class="w-20 h-7 bg-gray-300 rounded-md dark:bg-gray-700 m-1"></div>
+                    </div>
+                {/each}
+            {/if}
+        </div>
+    </Label>
+
+    <Label for="attachment" class="space-y-2 mt-4">
+        <div>Attachments</div>
+        <input bind:this={fileInput} type="file" accept="image/*" multiple hidden bind:files>
+        <Button gradient color="green" size="sm" type="button" on:click={() => fileInput.click()}>Add Pictures</Button>
+    </Label>
+
+    <div class="flex items-center justify-end gap-x-2 mt-4">
+        <Button color="dark" size="sm" type="button" href="/">Cancel</Button>
+        <Button color="green" size="sm" type="submit">Submit</Button>
+    </div>
+</div>
