fc4ce0433da030dcba27330d8d3e91b6880a8b77
Kinkando <TY_Sage@hotmail.com>
2023-02-23 20:09:20 +0700

    chore: add fetch data on page server

--- src/services/category.ts
+++ src/services/category.ts
@@ -36,3 +36,33 @@ export async function getAllCategories() {
     await new Promise(resolve => setTimeout(() => resolve(""), 500))
     return categories
 }
+
+export async function getCategoryByName(categoryName: string): Promise<Category|undefined> {
+    const categories: Category[] = [
+        {
+            categorySeq: 1,
+            categoryUUID: "",
+            categoryName: "Lifestyle",
+            categoryHexColor: "#86D97F",
+        },
+        {
+            categorySeq: 2,
+            categoryUUID: "",
+            categoryName: "Sport",
+            categoryHexColor: "#4C52E0",
+        },
+        {
+            categorySeq: 3,
+            categoryUUID: "",
+            categoryName: "Food",
+            categoryHexColor: "#857800",
+        }
+    ]
+
+    switch (categoryName) {
+        case "123": return categories[0]
+        case "456": return categories[1]
+        case "789": return categories[2]
+        default: return undefined
+    }
+}
