1f718e1d001921dadc8266abd845d8f276d0ab7c
Kinkando <TY_Sage@hotmail.com>
2023-02-25 17:54:46 +0700

    fix: position of loading spinner to center of screen

--- src/components/spinner/LoadingSpinner.svelte
+++ src/components/spinner/LoadingSpinner.svelte
@@ -6,7 +6,8 @@
 
 {#if isLoading}
     <!-- <div class="z-50 fixed w-screen h-screen top-0 left-0 flex justify-center items-center backdrop-brightness-50"> -->
-    <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20">
+    <!-- <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20"> -->
+    <div class="z-50 fixed w-screen h-screen top-0 left-0 flex justify-center items-center">
         <RingLoader color="green" size="100" />
     </div>
 {/if}
