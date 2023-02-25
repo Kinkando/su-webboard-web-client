eec0a1a1ec1930ef665fcda7daa83e3b23c2e011
Kinkando <TY_Sage@hotmail.com>
2023-02-25 12:20:34 +0700

    fix: overline of home section header title

--- src/components/partials/HomeSectionHeader.svelte
+++ src/components/partials/HomeSectionHeader.svelte
@@ -15,6 +15,13 @@
     </figure>
     <span class="uppercase">{title}</span>
     <span class="text-xs text-gray-400 hidden md:block">{description}</span>
-    <Button class="ml-auto uppercase" color="green" gradient on:click={action} href={href}>{buttonName}</Button>
+    <Button class="ml-auto uppercase {href ? 'max-[400.1px]:hidden' : ''}" size="xs" color="green" gradient on:click={action} href={href}>{buttonName}</Button>
+    {#if href}
+        <a class="ml-auto rounded-full bg-[var(--primary-color-75)] dark:bg-gray-700 p-1 min-[400.1px]:hidden" {href}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+            </svg>
+        </a>
+    {/if}
 </div>
 <hr class="my-2 border-gray-400">
