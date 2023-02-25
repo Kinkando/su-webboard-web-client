6dc7d8ada512f497d546b8ace2875a8ed1afb2eb
Kinkando <TY_Sage@hotmail.com>
2023-02-25 10:29:06 +0700

    feat: add tooltip and transition on pop over

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     import "../app.postcss";
-	import { fly } from 'svelte/transition';
+	import { fly, slide } from 'svelte/transition';
     import { page } from '$app/stores';
 	import { goto } from "$app/navigation";
-	import { DarkMode, Indicator, Input, Popover, ButtonGroup, Button } from "flowbite-svelte";
+	import { DarkMode, Indicator, Input, Popover, ButtonGroup, Button, Tooltip } from "flowbite-svelte";
     import type { Notification } from "@models/notification";
 	import type { User } from "@models/user";
 	import { UserType } from "@models/auth";
@@ -53,6 +53,33 @@
     export let data: any;
     const notification: Notification = data.notification;
     const user: User = data.user;
+
+    const tooltips = [
+        {
+            id: "theme-toggle",
+            text: "เปลี่ยนโหมด"
+        },
+        {
+            id: "search",
+            text: "ค้นหากระทู้",
+        },
+        {
+            id: "announcement",
+            text: "สร้างประกาศ",
+        },
+        {
+            id: "forum",
+            text: "สร้างกระทู้",
+        },
+        {
+            id: "notification",
+            text: "การแจ้งเตือน",
+        },
+        {
+            id: "profile",
+            text: "ผู้ใช้",
+        },
+    ]
 </script>
 
 <svelte:head>
@@ -61,9 +88,17 @@
 </svelte:head>
 
 {#if $page.status === HTTP.StatusOK && isUserSite}
-    <Popover placement="bottom" class="z-30 w-64 text-sm font-light min-[820.1px]:hidden" triggeredBy="#search" trigger="click">
+    {#each tooltips as tooltip}
+        {#if tooltip.id !== 'announcement' || user.userType === UserType.TEACHER}
+            <Tooltip triggeredBy="#{tooltip?.id}" shadow trigger="hover" placement="bottom" class="z-30 transition-colors ease-in duration-200 !text-black !bg-[var(--primary-color-75)] dark:!text-white dark:!bg-gray-700">
+                {tooltip?.text}
+            </Tooltip>
+        {/if}
+    {/each}
+
+    <Popover placement="bottom" class="z-30 w-64 text-sm font-light min-[820.1px]:hidden" shadow triggeredBy="#{tooltips[1].id}" trigger="click" transition={slide} params={{duration: 200}}>
         <Input
-            id="search"
+            id={tooltips[1].id}
             class="w-full"
             placeholder="ค้นหากระทู้ได้ที่นี่..."
             size="md"
@@ -77,7 +112,7 @@
         </Input>
     </Popover>
 
-    <Popover defaultClass="overflow-hidden w-fit" placement="bottom" class="z-30 w-fit text-sm text-black text-black dark:text-white font-light" triggeredBy="#notification" trigger="click">
+    <Popover defaultClass="overflow-hidden w-fit" placement="bottom" class="z-30 w-fit text-sm text-black text-black dark:text-white font-light" shadow triggeredBy="#{tooltips[4].id}" trigger="click" transition={slide} params={{duration: 200}}>
         <header class="relative text-center text-lg flex items-center justify-center gap-x-1 rounded-t-md py-1">
             <span class="">การแจ้งเตือน</span>
             {#if notification?.unreadNotiCount}
@@ -109,7 +144,7 @@
         </section>
     </Popover>
 
-    <Popover defaultClass="overflow-hidden py-2" placement="bottom" class="z-30 w-fit border-none text-sm text-black dark:text-white font-light -px-3" triggeredBy="#profile" trigger="click">
+    <Popover defaultClass="overflow-hidden py-2" placement="bottom" class="z-30 w-fit border text-sm text-black dark:text-white font-light -px-3" shadow triggeredBy="#{tooltips[5].id}" trigger="click" transition={slide} params={{duration: 200}}>
         <a class="flex items-center gap-x-3 cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700 px-3 py-2" href="/profile">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
@@ -135,7 +170,7 @@
         </a>
 
         <nav class="flex items-center gap-x-2 ml-auto">
-            <DarkMode btnClass="scale-110 text-white hover:bg-white hover:text-[var(--primary-color)] dark:hover:bg-gray-700 dark:hover:text-white rounded-full p-2 transition-all ease-in duration-200" />
+            <DarkMode id={tooltips[0].id} btnClass="scale-110 text-white hover:bg-white hover:text-[var(--primary-color)] dark:hover:bg-gray-700 dark:hover:text-white rounded-full p-2 transition-all ease-in duration-200" />
 
             <!-- INPUT SEARCH -->
             <div class="max-[820.1px]:hidden min-w-[300px]">
@@ -154,13 +189,13 @@
             </div>
 
             <!-- SEARCH ICON -->
-            <figure id="search" class="rounded-full text-white hover:bg-white hover:text-[var(--primary-color)] dark:hover:bg-gray-700 dark:hover:text-white p-1 w-10 h-10 relative cursor-pointer min-[820.1px]:hidden transition-all ease-in duration-200">
+            <figure id="{tooltips[1].id}" class="rounded-full text-white hover:bg-white hover:text-[var(--primary-color)] dark:hover:bg-gray-700 dark:hover:text-white p-1 w-10 h-10 relative cursor-pointer min-[820.1px]:hidden transition-all ease-in duration-200">
                 <svg class="w-full h-full" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
             </figure>
 
             <!-- NEW ANNOUNCEMENT ICON -->
             {#if user?.userType === UserType.TEACHER}
-                <a class="rounded-full text-white hover:bg-white hover:text-[var(--primary-color)] dark:hover:bg-gray-700 dark:hover:text-white p-1 w-10 h-10 relative cursor-pointer transition-all ease-in duration-200" href="/announcement/new">
+                <a id="{tooltips[2].id}" class="rounded-full text-white hover:bg-white hover:text-[var(--primary-color)] dark:hover:bg-gray-700 dark:hover:text-white p-1 w-10 h-10 relative cursor-pointer transition-all ease-in duration-200" href="/announcement/new">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46" />
                     </svg>
@@ -172,7 +207,7 @@
             {/if}
 
             <!-- NEW FORUM ICON -->
-            <a class="rounded-full text-white hover:bg-white hover:text-[var(--primary-color)] dark:hover:bg-gray-700 dark:hover:text-white p-1 w-10 h-10 relative cursor-pointer transition-all ease-in duration-200" href="/forum">
+            <a id="{tooltips[3].id}" class="rounded-full text-white hover:bg-white hover:text-[var(--primary-color)] dark:hover:bg-gray-700 dark:hover:text-white p-1 w-10 h-10 relative cursor-pointer transition-all ease-in duration-200" href="/forum">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
                 </svg>
@@ -183,7 +218,7 @@
             </a>
 
             <!-- NOTIFICATION ICON -->
-            <figure id="notification" class="rounded-full text-white hover:bg-white hover:text-[var(--primary-color)] dark:hover:bg-gray-700 dark:hover:text-white p-1 w-10 h-10 cursor-pointer -ml-1 relative transition-all ease-in duration-200">
+            <figure id="{tooltips[4].id}" class="rounded-full text-white hover:bg-white hover:text-[var(--primary-color)] dark:hover:bg-gray-700 dark:hover:text-white p-1 w-10 h-10 cursor-pointer -ml-1 relative transition-all ease-in duration-200">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.4" stroke="currentColor" class="w-full h-full">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                 </svg>
@@ -196,7 +231,7 @@
             </figure>
 
             <!-- USER AVATAR -->
-            <figure class="w-fit h-fit cursor-pointer" id="profile">
+            <figure id="{tooltips[5].id}" class="w-fit h-fit cursor-pointer">
                 <img
                     alt=""
                     class="rounded-50 w-10"
