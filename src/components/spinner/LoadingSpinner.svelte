056e80d4ff8d16f36c1050fff442daeda0a88465
Kinkando <TY_Sage@hotmail.com>
2023-02-25 16:01:40 +0700

    refactor: resize loading on login

--- src/components/spinner/LoadingSpinner.svelte
+++ src/components/spinner/LoadingSpinner.svelte
@@ -5,6 +5,7 @@
 </script>
 
 {#if isLoading}
+    <!-- <div class="z-50 fixed w-screen h-screen top-0 left-0 flex justify-center items-center backdrop-brightness-50"> -->
     <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20">
         <RingLoader color="green" size="100" />
     </div>
