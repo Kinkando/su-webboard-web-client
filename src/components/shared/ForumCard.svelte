df85333b953d55edbd92dd28e0e10877b38cf452
Kinkando <TY_Sage@hotmail.com>
2023-02-26 17:24:38 +0700

    enhancement: redraw UI for popular forum card

--- src/components/shared/ForumCard.svelte
+++ src/components/shared/ForumCard.svelte
@@ -8,23 +8,25 @@
     export let forum: Forum;
     const rankingColor = (() => {
         switch (forum?.ranking) {
-            case 1: return "background-color: #FFD700; color: #000"
-            case 2: return "background-color: #C0C0C0; color: #000"
-            case 3: return "background-color: #b87333; color: #000"
+            case 1: return "background-color: #FFD700 !important; color: #000 !important"
+            case 2: return "background-color: #C0C0C0 !important; color: #000 !important"
+            case 3: return "background-color: #b87333 !important; color: #000 !important"
             default: return ""
         }
     })()
 </script>
 
 <a class="bg-white text-black dark:bg-gray-700 dark:text-white hover:bg-gray-300 dark:hover:bg-gray-900 cursor-pointer p-4 sm:p-6 shadow-md drop-shadow-md rounded-md ease-in duration-200 w-full h-full flex gap-x-4" href="/forum/{forum?.forumUUID}">
-    {#if forum?.ranking}
-        <div class="flex items-center -mr-2">
-            <div class="w-8 h-8 p-1 rounded-full text-center" style={rankingColor}>
-                {forum?.ranking}
+    <div class="relative w-fit h-fit">
+        {#if forum?.ranking}
+            <div class="flex items-center -mr-2 absolute bottom-0 right-0">
+                <div class="w-6 h-6 p-1 rounded-full flex items-center justify-center !bg-gray-200 dark:!bg-gray-800" style={rankingColor}>
+                    {forum?.ranking}
+                </div>
             </div>
-        </div>
-    {/if}
-    <img src={forum?.authorImageURL} alt="" class="w-16 h-16">
+        {/if}
+        <img src={forum?.authorImageURL} alt="" class="min-w-[4rem] max-w-[4rem]">
+    </div>
     <section class="w-full max-w-full overflow-hidden flex flex-col gap-y-1">
         <div class="font-bold text-lg overflow-hidden text-ellipsis whitespace-nowrap">{forum?.title}</div>
         <div class="text-md overflow-hidden text-ellipsis whitespace-nowrap">{forum?.authorName}</div>
--- src/components/skeleton-load/SkeletonForumCard.svelte
+++ src/components/skeleton-load/SkeletonForumCard.svelte
@@ -5,14 +5,13 @@
 <div class="no-select ease-in duration-200 bg-gray-50 dark:bg-gray-700 text-black dark:text-gray-400 rounded-md border border-gray-200 dark:border-gray-700 shadow-md drop-shadow-md flex flex-col w-full h-full overflow-hidden">
     <div class="animate-pulse w-full h-full p-4 sm:p-6">
         <div class="flex">
-            {#if ranking}
-                <div class="flex items-center -mr-2 mt-2">
-                    <div class="w-8 h-8 bg-gray-300 rounded-full dark:bg-gray-600 mb-2.5"></div>
-                </div>
-            {/if}
-
-            <div class="-mt-2">
-                <svg class="inline-block w-20 h-20 {ranking ? 'ml-2' : '-ml-2'} mr-2 text-gray-300 dark:text-gray-600" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
+            <div class="-mt-2 relative w-fit h-fit">
+                {#if ranking}
+                    <div class="flex items-center absolute bottom-0 right-1.5">
+                        <div class="w-6 h-6 bg-gray-300 rounded-full border border-gray-50 dark:border-gray-700 dark:bg-gray-600 mb-2.5"></div>
+                    </div>
+                {/if}
+                <svg class="inline-block w-20 h-20 -ml-2 mr-2 text-gray-300 dark:text-gray-600" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
             </div>
 
             <section class="flex flex-col gap-y-2 flex-auto">
