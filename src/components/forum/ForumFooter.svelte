e2ab772979f998afb96306783744ab29782dfc90
Kinkando <TY_Sage@hotmail.com>
2023-02-28 02:53:26 +0700

    fix: text overlap on reply button

--- src/components/forum/ForumFooter.svelte
+++ src/components/forum/ForumFooter.svelte
@@ -27,14 +27,14 @@
 
 <hr class="mt-6 mb-3 dark:border-gray-500">
 <div class="flex justify-between items-center">
-    <div class="flex items-center">
+    <div class="flex items-center overflow-hidden">
         <img src={userImageURL} alt="" class="w-16 h-16">
-        <div class="ml-3 space-y-1">
-            <div class="font-bold">{username}</div>
-            <div class="font-light text-sm">2 ชั่วโมง</div>
+        <div class="ml-3 space-y-1 overflow-hidden mr-4">
+            <div class="font-bold overflow-hidden text-ellipsis whitespace-nowrap">{username}</div>
+            <div class="font-light text-sm overflow-hidden text-ellipsis whitespace-nowrap">2 ชั่วโมง</div>
         </div>
     </div>
     {#if commentCount}
-        <div class="underline text-[var(--primary-color)] dark:text-[var(--primary-color-75)] cursor-pointer">ตอบกลับ</div>
+        <div class="no-select underline text-[var(--primary-color)] dark:text-[var(--primary-color-75)] cursor-pointer whitespace-nowrap">ตอบกลับ</div>
     {/if}
 </div>
