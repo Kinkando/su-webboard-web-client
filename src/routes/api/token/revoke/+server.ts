f5ced4bfa981d6c3954842cb86c37f56b621487f
Kinkando <TY_Sage@hotmail.com>
2023-03-15 13:10:47 +0700

    feat: integrate API Revoke Token

--- src/routes/api/token/revoke/+server.ts
+++ src/routes/api/token/revoke/+server.ts
@@ -1,8 +1,15 @@
+import { revokeToken } from "@services/authen";
 import { json } from "@sveltejs/kit";
-import { revokeToken } from "@util/cookies";
+import { getToken, revokeToken as revokeCookie } from "@util/cookies";
 import type { RequestHandler } from "./$types";
 
 export const POST: RequestHandler = async ({ cookies }) => {
-    revokeToken(cookies)
+    try {
+        const token = getToken(cookies)
+        if (token) {
+            revokeToken(token.accessToken, token.refreshToken)
+        }
+    } catch (error) {}
+    revokeCookie(cookies)
     return json({ message: "success" })
 }
