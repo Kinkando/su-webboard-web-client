b1657592dc0b7ba2e9fa83cf2ca775b4e7c63c7f
Kinkando <TY_Sage@hotmail.com>
2023-03-15 18:10:24 +0700

    enhancement: remove related API Cookies usage

--- src/routes/api/user/profile/+server.ts
+++
@@ -1,8 +0,0 @@
-import { getUserProfile } from "@services/user";
-import { json } from "@sveltejs/kit";
-import type { RequestHandler } from "./$types";
-
-export const GET: RequestHandler = async ({ cookies }) => {
-    const res = await getUserProfile(cookies)
-    return json(res)
-}
