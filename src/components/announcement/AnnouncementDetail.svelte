85372d46d757053381cd49e1a71d656c3f5b881b
Kinkando <TY_Sage@hotmail.com>
2023-04-08 15:17:05 +0700

    fix: description of announcement textarea and add realtime created/updated time text

--- src/components/announcement/AnnouncementDetail.svelte
+++ src/components/announcement/AnnouncementDetail.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-	import ForumImage from "../forum/ForumImage.svelte";
+	import { onDestroy } from "svelte";
+	import { goto } from "$app/navigation";
+	import AnnouncementFooter from "./AnnouncementFooter.svelte";
+	import ForumImage from "@components/forum/ForumImage.svelte";
     import EllipsisMenu from "@components/shared/EllipsisMenu.svelte";
 	import type { Announcement, AnnouncementRequest } from "@models/announcement";
 	import type { Attachment, FormSchema } from "@models/new-post";
-	import { getUserUUID } from "@util/localstorage";
 	import { deleteAnnouncement, upsertAnnouncement } from "@services/announcement";
-	import { createEventDispatcher } from "svelte";
-	import AnnouncementFooter from "./AnnouncementFooter.svelte";
-	import { goto } from "$app/navigation";
+	import { timeRange } from "@util/datetime";
+	import { getUserUUID } from "@util/localstorage";
 
     export let announcementDetail: Announcement;
 
@@ -36,8 +37,6 @@
 
     let imageURLs = initImages()
 
-    const dispatch = createEventDispatcher()
-
     const editAnnouncementAction = async(titleEdit: string, descriptionEdit: string, attachmentsEdit: Attachment[], deleteImageUUIDs: string[]) => {
         const files = attachmentsEdit.map(attachment => attachment.file)
         const announcement: AnnouncementRequest = {
@@ -51,6 +50,7 @@
         // loading edit data
         announcementDetail.title = titleEdit;
         announcementDetail.description = descriptionEdit;
+        announcementDetail.updatedAt = new Date();
         title.value = titleEdit;
         description.value = descriptionEdit;
         if (deleteImageUUIDs && announcementDetail.announcementImages) {
@@ -72,6 +72,19 @@
     }
 
     $: userUUID = getUserUUID()
+
+    let createdAt = timeRange(announcementDetail.createdAt)
+    let updatedAt = announcementDetail.updatedAt ? timeRange(announcementDetail.updatedAt) : ''
+    const period = setInterval(() => {
+        createdAt = timeRange(announcementDetail.createdAt)
+        if (announcementDetail.updatedAt) {
+            updatedAt = timeRange(announcementDetail.updatedAt)
+        }
+    }, 1000)
+    onDestroy(() => clearInterval(period))
+    $: if(announcementDetail.updatedAt !== undefined) {
+        updatedAt = timeRange(announcementDetail.updatedAt)
+    }
 </script>
 
 <div class="rounded-lg shadow-md w-full h-full p-4 sm:p-6 overflow-hidden bg-white text-black dark:bg-gray-700 dark:text-white ease-in duration-200 max-w-5xl m-auto">
@@ -97,25 +110,29 @@
 
     <hr class="my-3 dark:border-gray-500">
     <div class="font-medium min-h-[12rem]">
-        {announcementDetail.description}
+        <span class="break-words">{@html announcementDetail?.description?.replaceAll('\n', '<br>') || ''}</span>
+        {#if imageURLs?.length}
+            <ForumImage bind:imageURLs />
+        {/if}
     </div>
 
-    {#if imageURLs?.length}
-        <ForumImage {imageURLs} />
-    {/if}
-
-    <div class="flex justify-end items-center mt-3 -mb-3">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-            <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
-            <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clip-rule="evenodd" />
-        </svg>
-        <div class="select-none ml-1 text-xs">{announcementDetail.seeCount} ผู้ชม</div>
+    <div class="flex {announcementDetail.updatedAt ? 'justify-between' :'justify-end'} items-center mt-3 -mb-3">
+        {#if announcementDetail.updatedAt}
+            <div class="text-gray-500 overflow-hidden text-ellipsis whitespace-nowrap">แก้ไขล่าสุด: {updatedAt || timeRange(announcementDetail.updatedAt)}</div>
+        {/if}
+        <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clip-rule="evenodd" />
+            </svg>
+            <div class="select-none ml-1 text-xs whitespace-nowrap">{announcementDetail.seeCount} ผู้ชม</div>
+        </div>
     </div>
 
     <AnnouncementFooter
-        userUUID={announcementDetail.authorUUID}
-        userDisplayName={announcementDetail.authorName}
-        userImageURL={announcementDetail.authorImageURL}
-        createdAt={announcementDetail.createdAt}
+        bind:userUUID={announcementDetail.authorUUID}
+        bind:userDisplayName={announcementDetail.authorName}
+        bind:userImageURL={announcementDetail.authorImageURL}
+        bind:createdAt
     />
 </div>
