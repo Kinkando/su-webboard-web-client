f1c73f3efa37b29f99771553479d25aa1cb8aaeb
Kinkando <TY_Sage@hotmail.com>
2023-04-08 10:52:33 +0700

    fix: display notification toast only once in each time

--- src/stores/toast.ts
+++ src/stores/toast.ts
@@ -5,11 +5,29 @@ const defaultToast: NotificationToast[] = [];
 
 export const toasts = writable<NotificationToast[]>(defaultToast);
 
+let total = 0;
+toasts.subscribe(notifications => total = notifications.length)
+
 export const pushNotification = (toast: NotificationToast) => {
     const id = Math.floor(Math.random() * 10000);
     toast.id = id;
-    toasts.update((all) => [toast, ...all]);
-    setTimeout(() => dismissToast(id), toast.timeout || 6000);
+    // toasts.update((all) => [toast, ...all]);
+    // setTimeout(() => dismissToast(id), toast.timeout || 6000);
+    setTimeout(async() => {
+        await new Promise(resolve => {
+            const loopOnce = setInterval(async () => {
+                if (total === 0) {
+                    await new Promise(resolve => setTimeout(() => resolve(""), 500))
+                    if (total === 0) {
+                        clearInterval(loopOnce)
+                        resolve("")
+                    }
+                }
+            }, 100)
+        })
+        toasts.update((all) => [toast, ...all]);
+        setTimeout(() => dismissToast(id), toast.timeout || 2000)
+    })
 }
 
 export const dismissToast = (id?: number) => {
